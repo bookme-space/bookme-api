f8f0d034065b69d821fb0c22157dc231633b5fb9
Andrey Shulmin <ashulmin14@gmail.com>
2024-03-24 17:23:37 +0300

    chore: update .prettierrc config

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app =
+    await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
   await app.listen(3000);
 }
 bootstrap();
