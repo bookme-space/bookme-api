7833e3f4ce0d51a612af4341e9ba80ae5305e387
Andrey Shulmin <ashulmin14@gmail.com>
2024-04-04 22:10:31 +0300

    feat: added als storage module

---
+++ src/core/modules/als/als.module.ts
@@ -0,0 +1,32 @@
+import { AsyncLocalStorage } from "node:async_hooks";
+
+import { DynamicModule } from "@nestjs/common";
+
+import { Als, IAls } from "./als.storage";
+
+interface AsyncContextModuleConfig {
+  isGlobal?: boolean;
+  alsInstance?: AsyncLocalStorage<Map<unknown, unknown>>;
+}
+
+export class AlsModule {
+  public static forRoot(
+    options?: AsyncContextModuleConfig,
+  ): DynamicModule {
+    const isGlobal = options?.isGlobal ?? false;
+    const alsInstance =
+      options?.alsInstance ?? new AsyncLocalStorage();
+
+    const alsProvider = {
+      provide: IAls,
+      useValue: new Als(alsInstance),
+    };
+
+    return {
+      module: AlsModule,
+      global: isGlobal,
+      providers: [alsProvider],
+      exports: [IAls],
+    };
+  }
+}
